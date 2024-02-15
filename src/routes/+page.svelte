239bc112dc2ea76ee74362a7d35e94bcf06b1cf5
Dat Nguyen <dnguyen@localhost.localdomain>
2024-02-15 13:38:07 -0800

    initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center h-full">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
